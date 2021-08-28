7039df564fab8798e0e6f8825989dbbee77b4980
jcloeter <johncloeter@gmail.com>
2021-08-27 22:41:42 -0500

    changed to IIFE

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-window.addEventListener("hashchange", function () {
-  //   console.log("hash changed");
-  getContent();
-});
+(function () {
+  window.addEventListener("hashchange", function () {
+    //   console.log("hash changed");
+    getContent();
+  });
 
-window.addEventListener("load", function () {
-  //   console.log("hash loaded");
-  getContent();
-});
+  window.addEventListener("load", function () {
+    //   console.log("hash loaded");
+    getContent();
+  });
 
-const cache = {};
+  const cache = {};
 
-function updateDisplay(text) {
-  const display = document.querySelector(".display");
-  display.innerHTML = "";
-  display.insertAdjacentHTML("afterbegin", text);
-}
+  function updateDisplay(text) {
+    const display = document.querySelector(".display");
+    display.innerHTML = "";
+    display.insertAdjacentHTML("afterbegin", text);
+  }
 
-function getContent() {
-  console.log(window.location.hash);
-  const file = window.location.hash.split("#")[1] + ".html";
+  function getContent() {
+    console.log(window.location.hash);
+    const file = window.location.hash.split("#")[1] + ".html";
 
-  if (cache.hasOwnProperty(file)) return updateDisplay(cache[file]);
-  const request = new XMLHttpRequest();
-  request.onload = function () {
-    // console.log(request.responseText);
-    cache[file] = request.responseText;
-    updateDisplay(request.responseText);
-  };
-  request.open("GET", file);
-  request.send(null);
-  console.log("😂🐌🐌🐌🐌");
-}
+    if (cache.hasOwnProperty(file)) return updateDisplay(cache[file]);
+    const request = new XMLHttpRequest();
+    request.onload = function () {
+      // console.log(request.responseText);
+      cache[file] = request.responseText;
+      updateDisplay(request.responseText);
+    };
+    request.open("GET", file);
+    request.send(null);
+  }
+})();
